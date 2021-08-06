a7d64a87230bb7051fc244e499b0a7d3f37ba148
SenorZero <lucaschmitz@live.de>
2021-08-06 11:50:10 +0200

    add QueryBuilder testcase to confirm correct behaviour when wrong order of month is input

--- src/test/java/com/example/MasterProjekt/QueryBuilderTest.java
+++ src/test/java/com/example/MasterProjekt/QueryBuilderTest.java
@@ -13,51 +13,73 @@ import org.junit.jupiter.api.Test;
 
 public class QueryBuilderTest {
 
-    @Test
-    public void buildCorrectQueryOverMultipleMonth() {
-        QueryBuilder queryBuilder = new QueryBuilder();
-        Map<YearMonth, String> resultQueryForEachMonth = new HashMap<YearMonth, String>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM");
+        @Test
+        public void buildCorrectQueryOverMultipleMonth() {
+                QueryBuilder queryBuilder = new QueryBuilder();
+                Map<YearMonth, String> resultQueryForEachMonth = new HashMap<YearMonth, String>();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM");
 
-        resultQueryForEachMonth.put(YearMonth.parse("2016_10", formatter),
-                "SELECT DISTINCT * FROM `httparchive.technologies." + "2016_10_01_desktop" + "` where url like '%."
-                        + "de" + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
-                        + "2016_10_15_desktop" + "` where url like '%." + "de" + "/' AND info !=''");
-        resultQueryForEachMonth.put(YearMonth.parse("2016_11", formatter),
-                "SELECT DISTINCT * FROM `httparchive.technologies." + "2016_11_01_desktop" + "` where url like '%."
-                        + "de" + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
-                        + "2016_11_15_desktop" + "` where url like '%." + "de" + "/' AND info !=''");
-        resultQueryForEachMonth.put(YearMonth.parse("2016_12", formatter),
-                "SELECT DISTINCT * FROM `httparchive.technologies." + "2016_12_01_desktop" + "` where url like '%."
-                        + "de" + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
-                        + "2016_12_15_desktop" + "` where url like '%." + "de" + "/' AND info !=''");
-        resultQueryForEachMonth.put(YearMonth.parse("2017_01", formatter),
-                "SELECT DISTINCT * FROM `httparchive.technologies." + "2017_01_01_desktop" + "` where url like '%."
-                        + "de" + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
-                        + "2017_01_15_desktop" + "` where url like '%." + "de" + "/' AND info !=''");
-        resultQueryForEachMonth.put(YearMonth.parse("2017_02", formatter),
-                "SELECT DISTINCT * FROM `httparchive.technologies." + "2017_02_01_desktop" + "` where url like '%."
-                        + "de" + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
-                        + "2017_02_15_desktop" + "` where url like '%." + "de" + "/' AND info !=''");
-        resultQueryForEachMonth.put(YearMonth.parse("2017_03", formatter),
-                "SELECT DISTINCT * FROM `httparchive.technologies." + "2017_03_01_desktop" + "` where url like '%."
-                        + "de" + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
-                        + "2017_03_15_desktop" + "` where url like '%." + "de" + "/' AND info !=''");
-        assertTrue(resultQueryForEachMonth
-                .equals(queryBuilder.getVulnerabilitiesInPeriodForCountryQuery("2016_10", "2017_03", "de")));
-    }
+                resultQueryForEachMonth.put(YearMonth.parse("2016_10", formatter),
+                                "SELECT DISTINCT * FROM `httparchive.technologies." + "2016_10_01_desktop"
+                                                + "` where url like '%." + "de"
+                                                + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
+                                                + "2016_10_15_desktop" + "` where url like '%." + "de"
+                                                + "/' AND info !=''");
+                resultQueryForEachMonth.put(YearMonth.parse("2016_11", formatter),
+                                "SELECT DISTINCT * FROM `httparchive.technologies." + "2016_11_01_desktop"
+                                                + "` where url like '%." + "de"
+                                                + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
+                                                + "2016_11_15_desktop" + "` where url like '%." + "de"
+                                                + "/' AND info !=''");
+                resultQueryForEachMonth.put(YearMonth.parse("2016_12", formatter),
+                                "SELECT DISTINCT * FROM `httparchive.technologies." + "2016_12_01_desktop"
+                                                + "` where url like '%." + "de"
+                                                + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
+                                                + "2016_12_15_desktop" + "` where url like '%." + "de"
+                                                + "/' AND info !=''");
+                resultQueryForEachMonth.put(YearMonth.parse("2017_01", formatter),
+                                "SELECT DISTINCT * FROM `httparchive.technologies." + "2017_01_01_desktop"
+                                                + "` where url like '%." + "de"
+                                                + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
+                                                + "2017_01_15_desktop" + "` where url like '%." + "de"
+                                                + "/' AND info !=''");
+                resultQueryForEachMonth.put(YearMonth.parse("2017_02", formatter),
+                                "SELECT DISTINCT * FROM `httparchive.technologies." + "2017_02_01_desktop"
+                                                + "` where url like '%." + "de"
+                                                + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
+                                                + "2017_02_15_desktop" + "` where url like '%." + "de"
+                                                + "/' AND info !=''");
+                resultQueryForEachMonth.put(YearMonth.parse("2017_03", formatter),
+                                "SELECT DISTINCT * FROM `httparchive.technologies." + "2017_03_01_desktop"
+                                                + "` where url like '%." + "de"
+                                                + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
+                                                + "2017_03_15_desktop" + "` where url like '%." + "de"
+                                                + "/' AND info !=''");
+                assertTrue(resultQueryForEachMonth.equals(
+                                queryBuilder.getVulnerabilitiesInPeriodForCountryQuery("2016_10", "2017_03", "de")));
+        }
 
-    @Test
-    public void buildCorrectQueryASingleMonth() {
-        QueryBuilder queryBuilder = new QueryBuilder();
-        Map<YearMonth, String> resultQueryForEachMonth = new HashMap<YearMonth, String>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM");
+        @Test
+        public void buildCorrectQueryASingleMonth() {
+                QueryBuilder queryBuilder = new QueryBuilder();
+                Map<YearMonth, String> resultQueryForEachMonth = new HashMap<YearMonth, String>();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM");
 
-        resultQueryForEachMonth.put(YearMonth.parse("2020_08", formatter),
-                "SELECT DISTINCT * FROM `httparchive.technologies." + "2020_08_01_desktop" + "` where url like '%."
-                        + "de" + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
-                        + "2020_08_15_desktop" + "` where url like '%." + "de" + "/' AND info !=''");
-        assertTrue(resultQueryForEachMonth
-                .equals(queryBuilder.getVulnerabilitiesInPeriodForCountryQuery("2020_08", "2020_08", "de")));
-    }
+                resultQueryForEachMonth.put(YearMonth.parse("2020_08", formatter),
+                                "SELECT DISTINCT * FROM `httparchive.technologies." + "2020_08_01_desktop"
+                                                + "` where url like '%." + "de"
+                                                + "/' AND info !='' union DISTINCT SELECT DISTINCT * FROM `httparchive.technologies."
+                                                + "2020_08_15_desktop" + "` where url like '%." + "de"
+                                                + "/' AND info !=''");
+                assertTrue(resultQueryForEachMonth.equals(
+                                queryBuilder.getVulnerabilitiesInPeriodForCountryQuery("2020_08", "2020_08", "de")));
+        }
+
+        @Test
+        public void buildNoQueryWithWrongOrderOfMonth() {
+                QueryBuilder queryBuilder = new QueryBuilder();
+                Map<YearMonth, String> emptyResultMap = new HashMap<YearMonth, String>();
+                assertTrue(emptyResultMap.equals(
+                                queryBuilder.getVulnerabilitiesInPeriodForCountryQuery("2020_10", "2020_08", "de")));
+        }
 }

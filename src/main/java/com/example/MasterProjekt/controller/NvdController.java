c79cadc894a1c3603f6899d60a5dff26846b493b
SenorZero <lucaschmitz@live.de>
2021-10-04 11:26:00 +0200

    chnage an absolute path to a relative one

--- src/main/java/com/example/MasterProjekt/controller/NvdController.java
+++ src/main/java/com/example/MasterProjekt/controller/NvdController.java
@@ -1,10 +1,12 @@
 package com.example.MasterProjekt.controller;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 import com.example.MasterProjekt.util.NvdJsonParser;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +17,8 @@ public class NvdController {
     NvdJsonParser nvdJsonParser;
 
     @GetMapping("/parseNVDToDatabase")
-    public void test2() throws InterruptedException {
-        File jsonDirectory = new File(
-                "D:\\Projekte\\Master-projekt\\MasterProjektBackend\\src\\main\\resources\\nvDJsons");
+    public void test2() throws InterruptedException, FileNotFoundException {
+        File jsonDirectory = ResourceUtils.getFile("classpath:nvdJsons");
         File[] jsonDirListing = jsonDirectory.listFiles();
         for (File json : jsonDirListing) {
             nvdJsonParser.parseNvdJson(json.toPath().toString().replace("\\", "\\\\"), jsonDirListing.length);
